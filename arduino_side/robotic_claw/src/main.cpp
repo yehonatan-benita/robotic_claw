03849606e92051d359fd058f8ac267efb02aa60e
yehonatanBenita <114101066+yehonatanBenita@users.noreply.github.com>
2024-08-17 14:56:08 +0300

    still bad habited servo open and close with comuter orders

--- arduino_side/robotic_claw/src/main.cpp
+++ arduino_side/robotic_claw/src/main.cpp
@@ -1,21 +1,26 @@
 #include <Arduino.h>
 #include "claw.hpp"
 
+#define BAUDRATE 9600
 #define SERVO_PWM_PIN 9
 // put function declarations here:
 Claw claw;
 
 void setup() {
+  Serial.begin(BAUDRATE);
   Servo claw_servo = Servo();
   claw_servo.attach(SERVO_PWM_PIN);
   claw = Claw(claw_servo);
 }
 
 void loop() {
-  claw.close();
-  delay(1000);
-  claw.open();
-  delay(1000);
+  String message = Serial.readString(); 
+  if(message == "1"){
+    claw.open();
+  }
+  else if(message == "0"){
+    claw.close();
+  }
 }
 
 // put function definitions here:
